203d950c89ece93cc44e40109755f3f5edcc396d
salah-sorour-dev <soroursalaheddine@gmail.com>
2025-09-23 23:06:59 +0100

    Add class Util && show Balances list

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,6 +3,7 @@
 #include "clsPerson.h"
 #include "clsBankClient.h"
 #include "clsInputValidate.h"
+#include "clsUtil.h"
 #include <iomanip>
 
 
@@ -196,6 +197,50 @@ void ShowClientsList()
     cout << "___________________________________________\n";
 }
 
+void PrintClientRecordBalanceLine(clsBankClient Client)
+{
+
+    cout << "| " << setw(15) << left << Client.AccountNumber();
+    cout << "| " << setw(40) << left << Client.FullName();
+    cout << "| " << setw(12) << left << Client.AccountBalance;
+
+}
+
+
+void ShowTotalBalances()
+{
+
+    vector <clsBankClient> vClients = clsBankClient::GetClientsList();
+
+    cout << "\n\t\t\t\t\tBalances List (" << vClients.size() << ") Client(s).";
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+
+    cout << "| " << left << setw(15) << "Accout Number";
+    cout << "| " << left << setw(40) << "Client Name";
+    cout << "| " << left << setw(12) << "Balance";
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+
+    double TotalBalances = clsBankClient::GetTotalBalances();
+
+    if (vClients.size() == 0)
+        cout << "\t\t\t\tNo Clients Available In the System!";
+    else
+
+        for (clsBankClient Client : vClients)
+        {
+            PrintClientRecordBalanceLine(Client);
+            cout << endl;
+        }
+
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+    cout << "\t\t\t\t\t   Total Balances = " << TotalBalances << endl;
+    cout << "\t\t\t\t\t   ( " << clsUtil::NumberToText(TotalBalances) << ")";
+}
+
+
 int main()
 {
     
@@ -205,8 +250,9 @@ int main()
 
     //DeleteClient();
 
-    ShowClientsList();
+    //ShowClientsList();
 
+    ShowTotalBalances();
 
     system("pause>0");
     return 0;
--- clsBankClient.h
+++ clsBankClient.h
@@ -366,5 +366,22 @@ public:
         return _LoadClientsDataFromFile();
     }
 
+    static float GetTotalBalances()
+    {
+        vector <clsBankClient> vClients = clsBankClient::GetClientsList();
+
+        double TotalBalances = 0;
+
+        for (clsBankClient Client : vClients)
+        {
+
+            TotalBalances += Client.AccountBalance;
+        }
+
+        return TotalBalances;
+
+    }
+
+
 };
 
